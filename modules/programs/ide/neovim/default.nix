0f9ea62219461dce94ded1d12f4f2d7c43db5640
NicolaiVdS <nicolai@nvds.be>
2023-10-02 17:40:48 +0200

    neovim config update

--- modules/programs/ide/neovim/default.nix
+++ modules/programs/ide/neovim/default.nix
@@ -7,23 +7,21 @@
     vimAlias = true;
     #vimdiffAlias = true;
 
-    defaultEditor = true;
-
     extraLuaConfig = ''
       ${builtins.readFile ./options.lua}
     '';
   };
   environment.variables.EDITOR = "nvim";
-  home-manager.users.nicolaivds = {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
-    home.packages = with pkgs; [
-      rnix-lsp
-    ];
-    home.sessionVariables = {
-      EDITOR = "nvim";
-    };
-  };
+  # home-manager.users.nicolaivds = {
+  #   programs.neovim = {
+  #     enable = true;
+  #     defaultEditor = true;
+  #   };
+  #   home.packages = with pkgs; [
+  #     rnix-lsp
+  #   ];
+  #   home.sessionVariables = {
+  #     EDITOR = "nvim";
+  #   };
+  # };
 }
